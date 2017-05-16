0717e1ad05453c3b8385595c86e41dd721250efc
JackeL <it.davidkaiser@gmail.com>
2017-05-16 11:34:29 -0700

    new hot commit... ouch that burns

--- wallpaper.js
+++ wallpaper.js
@@ -16,6 +16,7 @@ const clear = require('clear')
 let HOST = 'https://api.unsplash.com/'
 let lastarg = '2 '
 let clientkey = ''
+let automatic = false
 let change = false
 let imageInfo = {
   user: '',
@@ -25,12 +26,14 @@ const errorHandle = /errors\"\:\["(.*)"]}/g
 
 console.log(
   chalk.blue(
-    figlet.textSync('JackPaper', { horizontalLayout: 'full' })
-  )
+    figlet.textSync('JackPaper', {
+      horizontalLayout: 'full',
+      font: '3D-ASCII'
+    }))
 )
 
 commander
-.version('1.1.0')
+.version('1.2.3')
 .command('change')
 .description('Change Background')
 .action((q) => {
@@ -42,6 +45,26 @@ commander
 })
 
 commander
+.command('auto')
+.option('-t, --time <time>', 'adjust the time')
+.description('Change Background per adjusted time')
+.action((auto, time) => {
+  automatic = true
+  if (typeof(auto) !== 'object' && time.time) {
+    setInterval(() => {
+      applyImage(auto)
+    }, (time.time * 1000))
+  } else if (auto.time){
+    setInterval(() => {
+      applyImage()
+    }, (auto.time * 1000))
+  } else {
+    console.log('--time required. Example: ', chalk.bold.green('jackpaper auto --time 10'))
+    process.exit()
+  }
+})
+
+commander
 .command('key')
 .description('Set your UnSplash Developer ID')
 .action((key) => {
@@ -49,12 +72,29 @@ commander
 })
 
 commander.on('--help', () => {
+  clear()
+  console.log('Usage: jackpaper <command> [options]')
+  console.log('')
+  console.log('Commands:')
+  console.log('')
+  console.log('change                 Change Background')
+  console.log('auto <options>         Change Background per adjusted time')
+  console.log('key                    Set your UnSplash Developer ID')
+  console.log('')
+  console.log('Options:')
+  console.log('-t, --time     set time in seconds for auto command')
+  console.log('-h, --help     output usage information')
+  console.log('-V, --version  output the version number')
   console.log('  ')
   console.log('  Examples:')
   console.log('  ')
+  console.log('    $ jackpaper --version')
+  console.log('    $ jackpaper --help')
   console.log('    $ jackpaper key')
   console.log('    $ jackpaper change')
   console.log('    $ jackpaper change galaxy')
+  console.log('    $ jackpaper auto --time 10')
+  console.log('    $ jackpaper auto galaxy --time 10')
   console.log('')
 });
 commander.parse(process.argv)
@@ -81,19 +121,26 @@ function applyImage (query) {
     } else {
       clientkey = data
       getPhotos('random', query, (error, photos, link) => {
+        // if (error) console.log(error)
         if (!error) {
           download(photos.urls.full, './assets/image.jpg').then(() => {
-            process.exit()
+            if (!automatic) {
+              process.exit()
+            }
           })
         } else {
+          // console.log(error)
           let err = error.toString()
           let m = errorHandle.exec(err)
+          // console.log('m[1] = ', m[1])
           if (m[1] == "OAuth error: The access token is invalid") {
             console.log(chalk.bold.red('Your Key is Not Valid! Please Run this Command: ', chalk.bold.green('$ jackpaper key')))
           } else {
             console.log(m[1])
+            process.exit()
           }
-          process.exit()
+            process.exit()
+            clear()
         }
       })
     }
@@ -157,13 +204,20 @@ function getPhotos(page, query, callback) {
          'Content-type': 'application/json',
          'Authorization': 'Client-ID ' + clientkey
       }
-   },
-   (err, res, body) => {
-     let json = JSON.parse(body)
-     imageInfo.user = json.user.name
-     imageInfo.url = json.links.html
-      if (err) return callback(err)
-      if (res.statusCode !== 200) return callback(new Error(body), null)
-      return callback(null, JSON.parse(body), res.headers.link)
+    },
+    (err, res, body) => {
+      if (body == 'Rate Limit Exceeded') {
+        console.log(chalk.bold.red('Your rate limit has reached the max, allow up to an hour for it to refresh. Or put a new key in.'))
+        process.exit()
+      } else {
+        let json = JSON.parse(body)
+        if (json.user) {
+          imageInfo.user = json.user.name
+          imageInfo.url = json.links.html
+        }
+        if (err) return callback(err)
+        if (res.statusCode !== 200) return callback(new Error(body), null)
+        return callback(null, JSON.parse(body), res.headers.link)
+      }
    })
 }
